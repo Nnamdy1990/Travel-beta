378dcd5ce79848d83fcc8187ce9e812ad3d87d50
Nnamdy1990 <Nnamdy1990@users.noreply.github.com>
2020-07-17 13:27:57 +0100

    fixed-devs

--- server/config/devs.js
+++ server/config/devs.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://pnator:kelechi1990@cluster0-zcxeu.mongodb.net/online-shop?retryWrites=true&w=majority",
+    "mongodb+srv://pnator:kelechi1990@cluster0-zcxeu.mongodb.net/online-shop?retryWrites=true",
 };
